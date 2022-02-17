c72b86985ccb3061273f720f65f6c072700ff4ed
the-man-w-laughs <92399441+the-man-w-laughs@users.noreply.github.com>
2022-02-18 00:16:34 +0300

    Update test6.sh

--- test6.sh
+++ test6.sh
@@ -2,7 +2,6 @@ if [ $# -eq 3 ] ; then
 	find "$2" -name "*.$3" > "$1"
 	sort "$1"
 else
-	echo "Wrong number of parameters!" > &2
-	echo "Command format: (out file name) (search dir) (extension)" > &2
+	echo "Wrong number of parameters!"; echo "Command format: (out file name) (search dir) (extension)" > &2
 fi
 
