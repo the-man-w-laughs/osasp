44791bd700bb10bcee2f0dcbfdc1548ba40bfe73
the-man-w-laughs <92399441+the-man-w-laughs@users.noreply.github.com>
2022-02-17 15:55:41 +0300

    Add files via upload

--- laba1_6.sh
+++ laba1_6.sh
@@ -3,5 +3,5 @@ if [ $# -eq 4 ] ; then
 	printf "Number of viewed files:" ; find $1 -type f | wc -l
 else
 	echo "Wrong number of parameters!" > &2
-	echo "Command format: (directory) (min) (max) (out file)" > &2
+	echo "Command format: (serch directory) (min size) (max size) (out file name)" > &2
 fi
--- test7.sh
+++ test7.sh
@@ -1,10 +1,10 @@
 if [ $# -ne 2 ] ; then
 	echo "Wrong number of parameters!" > &2
-	echo "Command format: (infile) (outfile)" > &2
+	echo "Command format: (file with code) (compiled file name)" > &2
 else
-gcc "$1" -o "$2"
-if [ $? -eq 0 ] ; then 
-	./"$2"
-fi
+	gcc "$1" -o "$2"
+	if [ $? -eq 0 ] ; then 
+		./"$2"
+	fi
 fi
 
