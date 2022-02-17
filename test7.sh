b37feb24be2c9ce90b7766405671afcafadc9ca4
the-man-w-laughs <92399441+the-man-w-laughs@users.noreply.github.com>
2022-02-18 00:17:07 +0300

    Update test7.sh

--- test7.sh
+++ test7.sh
@@ -1,6 +1,5 @@
 if [ $# -ne 2 ] ; then
-	echo "Wrong number of parameters!" > &2
-	echo "Command format: (file with code) (compiled file name)" > &2
+	echo "Wrong number of parameters!"; echo "Command format: (file with code) (compiled file name)" > &2
 else
 	gcc "$1" -o "$2"
 	if [ $? -eq 0 ] ; then 
