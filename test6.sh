5451966c0b4af120cd5fbc02c60c9b66f24db98d
the-man-w-laughs <92399441+the-man-w-laughs@users.noreply.github.com>
2022-02-19 10:02:31 +0300

    Update test6.sh

--- test6.sh
+++ test6.sh
@@ -1,5 +1,5 @@
 if [ $# -eq 3 ] ; then 
-	find "$2" -name "*.$3" | sort "$1" -d >"$1"
+	find "$2" -name "*.$3" -printf " %f\n" | sort -d >"$1"
 else
 	echo "Wrong number of parameters!
 Command format: (out file name) (search dir) (extension)." >&2
