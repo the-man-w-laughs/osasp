bad2138b4c287db0276a7803f72ec0ba02828b8d
the-man-w-laughs <92399441+the-man-w-laughs@users.noreply.github.com>
2022-03-04 09:08:44 +0300

    Delete test6.sh

--- test6.sh
+++
@@ -1,7 +0,0 @@
-if [ $# -eq 3 ] ; then 
-	find "$2" -name "*.$3" -printf "%f\n" | sort -d >"$1"
-else
-	echo "Wrong number of parameters!
-Command format: (out file name) (search dir) (extension)." >&2
-fi
-
